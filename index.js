afad5b39fb496fa3f48dd3ac502ca4a6b1f51cc4
benrobo <alumonabenaiah71@gmail.com>
2020-08-01 17:00:00 +0100

    first

--- index.js
+++ index.js
@@ -32,18 +32,21 @@ function pausesongt(){
 }
 
 
-// play song rom the list
-// var list = [
-//     "welcome",
-//     "back",
-// ]
-// var ul = document.getElementsByTagName("ul")
-// var list = document.createElement("li").appendChild(p);
+// music albumns script
 
-// var p = document.createTextNode(list);
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// list;
-// ul
-// console.log(ul);
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 
 
